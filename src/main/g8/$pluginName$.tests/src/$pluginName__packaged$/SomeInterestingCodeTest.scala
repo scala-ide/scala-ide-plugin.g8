aac6b6d2b42a24560a08f8e10b75f99e75acb5c9
Luc Bourlier <luc.bourlier@typesafe.com>
2012-07-24 11:55:33 +0200

    Fixed compilation order and 2.0.2 site location
    Fixed some labels
    Removed description property (it was not working, and not really needed)

--- src/main/g8/$pluginName$.tests/src/$pluginName__packaged$/SomeInterestingCodeTest.scala
+++ src/main/g8/$pluginName$.tests/src/$pluginName__packaged$/SomeInterestingCodeTest.scala
@@ -4,7 +4,7 @@ import org.junit.Test
 import org.junit.Assert._
 import scala.tools.eclipse.testsetup.TestProjectSetup
 
-class SomeInterestingCodeTest extends TestProjectSetup("aProject", bundleName= "$pluginName$") {
+class SomeInterestingCodeTest extends TestProjectSetup("aProject", bundleName= "$pluginName$.tests") {
   
   @Test
   def numberOfTypes() {
